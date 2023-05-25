0ff42bbaf28c2e3aa66db3bded0869bdc9866a2c
daniBoy1111 <129314741+daniBoy1111@users.noreply.github.com>
2023-05-25 00:18:40 -0500

    actualización

---
+++ picada.js/sumatoria.js
@@ -0,0 +1,37 @@
+const table = document.getElementById('picada-table');
+    const rows = document.getElementsByTagName('tr');
+
+    for (let i = 1; i < rows.length; i++) {
+      const row = rows[i];
+      const price = parseFloat(row.cells[1].innerText);
+      const quantityInput = row.cells[2].getElementsByTagName('input')[0];
+      const totalCell = row.cells[3];
+
+      quantityInput.addEventListener('input', updateTotal);
+
+      function updateTotal() {
+        const quantity = parseInt(quantityInput.value);
+        const total = price * quantity;
+
+        totalCell.textContent = total;
+        calculateTotal();
+      }
+    }
+
+    function calculateTotal() {
+      let totalSum = 0;
+
+      for (let i = 1; i < rows.length; i++) {
+        const row = rows[i];
+        const totalCell = row.cells[3];
+        const total = parseFloat(totalCell.textContent);
+
+        if (!isNaN(total)) {
+          totalSum += total;
+        }
+      }
+
+      const totalRow = table.rows[table.rows.length - 1];
+      const totalCell = totalRow.cells[3];
+      totalCell.textContent = totalSum;
+    }
